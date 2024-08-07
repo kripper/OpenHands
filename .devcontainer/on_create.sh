5891236e9c22680da3b5961e1895c03e280ae243
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-08-07 17:28:39 +0530

    add persist_sandbox to devcontainer

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -6,3 +6,9 @@ sudo apt install -y python3.11
 curl -sSL https://install.python-poetry.org | python3.11 -
 # chromadb requires SQLite > 3.35 but SQLite in Python3.11.9 comes with 3.31.1
 sudo cp /opt/conda/lib/libsqlite3.so.0 /lib/x86_64-linux-gnu/libsqlite3.so.0
+cat << EOF > config.toml
+[core]
+workspace_base = "./workspace"
+persist_sandbox = 1
+ssh_password = "a"
+EOF
