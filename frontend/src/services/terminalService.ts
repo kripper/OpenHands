8a7fcccc93b4ef36b075069b4b24044daa93874f
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-08-21 19:22:30 +0530

    update regext

--- frontend/src/services/terminalService.ts
+++ frontend/src/services/terminalService.ts
@@ -2,9 +2,9 @@ import ActionType from "#/types/ActionType";
 import Session from "./session";
 
 export function sendTerminalCommand(command: string): void {
-  // replace END OF TEXT character copied from terminal
+  // replace ^c character when copied from terminal
   // eslint-disable-next-line no-control-regex
-  const cleanedCommand = command.replace(/\u0003+/, "");
+  const cleanedCommand = command.replace(/\u0003\b/, "");
   if (!cleanedCommand) return;
   const event = { action: ActionType.RUN, args: { command: cleanedCommand } };
   const eventString = JSON.stringify(event);
