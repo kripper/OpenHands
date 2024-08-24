3c34519feaf3b1bd92675450b5a5b9ff3eb06d33
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-08-24 15:35:27 +0530

    Update sandbox_checker.py

--- sandbox_checker.py
+++ sandbox_checker.py
@@ -6,7 +6,7 @@ data = {
     'action': {
         'action': 'run_ipython',
         'args': {
-            'code': 'open_file',
+            'code': '%cd /workspace && %pwd',
         },
     }
 }
@@ -20,10 +20,43 @@ data1 = {
     }
 }
 
-json_data = json.dumps(data)
-response = requests.post('http://localhost:63711/execute_action', json=data, timeout=1)
-try:
-    print(response.json()['content'])
-except Exception:
-    print(response.json())
-    print(response.status_code)
+
+def run_ipython(code):
+    return {
+        'action': {
+            'action': 'run_ipython',
+            'args': {
+                'code': code,
+            },
+        }
+    }
+
+
+def run(command):
+    return {
+        'action': {
+            'action': 'run',
+            'args': {
+                'command': command,
+            },
+            'timeout': 1,
+        }
+    }
+
+
+def execute_action(data):
+    data = json.dumps(data)
+    try:
+        response = requests.post(
+            'http://localhost:63711/execute_action', data=data, timeout=1
+        )
+        print(response.json()['content'])
+    except Exception:
+        print(response.json())
+        print(response.status_code)
+
+
+# execute_action(run_ipython('%cd /workspace/astropy__astropy__4.3'))
+execute_action(run_ipython('%pwd'))
+execute_action(run_ipython('edit_file'))
+# execute_action(run('pwd'))
