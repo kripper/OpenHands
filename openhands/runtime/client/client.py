f51e42f9cedd92750351af5f432f00ad0c04284b
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-09-08 10:28:34 +0530

    debug kernel_init_code

--- openhands/runtime/client/client.py
+++ openhands/runtime/client/client.py
@@ -440,7 +440,7 @@ class RuntimeClient:
 
         await self.chdir()
         # re-init the kernel after restart
-        logger.info('Re-initializing the kernel...')
+        logger.info(f'Re-initializing the kernel with {self.kernel_init_code}')
         act = IPythonRunCellAction(code=self.kernel_init_code)
         obs = await jupyter_plugin.run(act)
         logger.info(f'Kernel re-initialized. Output: {obs}')
