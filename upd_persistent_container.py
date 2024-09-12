e4bd990eecfb38b86e469de42017125350b9f039
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-09-12 21:05:02 +0530

    Update upd_persistent_container.py

--- upd_persistent_container.py
+++ upd_persistent_container.py
@@ -1,22 +1,29 @@
-import shutil
+import os
+import subprocess
 
 import docker
 
-a = r'openhands\runtime\client\client.py'
-b = r'workspace\client.py'
+cmd = 'docker cp openhands/runtime/plugins/agent_skills/file_ops/file_ops.py 880cdf72bd6cc256c17797ad1baab2c121e0d1423cff13c114d97fbf9435af3f:/openhands/code/openhands/runtime/plugins/agent_skills/file_ops/file_ops.py'
+print(subprocess.run(cmd, shell=0, capture_output=True).stdout)
+# find why empty output
+exit()
 
-shutil.copy(a, b)
-
+full_file_name = 'openhands/runtime/plugins/agent_skills/file_ops/file_ops.py'
+# full_file_name = r'openhands/runtime/client/client.py'
+print(os.getcwd())
 client = docker.from_env()
 for c in client.containers.list():
     if 'persisted' in c.name:
         print(c.name)
-        break
-o = c.exec_run(
-    'mv /workspace/client.py /openhands/code/openhands/runtime/client/client.py'
-)
-print(o.output.decode('utf-8'))
+        # break
+        # copy the file to the container
+        container_id = c.id
+        print(container_id)
+        cmd = f'docker cp {full_file_name} {container_id}:/openhands/code/{full_file_name}'
+        print(cmd)
+        out = subprocess.run(cmd.split(' '), shell=0, capture_output=True)
+        print(out.stdout, out.stderr)
 
 # restart the container
-print('Restarting the container')
-c.restart()
+# print('Restarting the container')
+# c.restart()
