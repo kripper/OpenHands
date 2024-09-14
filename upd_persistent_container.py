44e0dedceaa37e2f122f25d0fda2cdc1df4d1fa5
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-09-14 17:33:34 +0530

    Update upd_persistent_container.py

--- upd_persistent_container.py
+++ upd_persistent_container.py
@@ -6,10 +6,9 @@ import docker
 cmd = 'docker cp openhands/runtime/plugins/agent_skills/file_ops/file_ops.py 880cdf72bd6cc256c17797ad1baab2c121e0d1423cff13c114d97fbf9435af3f:/openhands/code/openhands/runtime/plugins/agent_skills/file_ops/file_ops.py'
 print(subprocess.run(cmd, shell=0, capture_output=True).stdout)
 # find why empty output
-exit()
 
 full_file_name = 'openhands/runtime/plugins/agent_skills/file_ops/file_ops.py'
-# full_file_name = r'openhands/runtime/client/client.py'
+full_file_name = r'openhands/runtime/client/client.py'
 print(os.getcwd())
 client = docker.from_env()
 for c in client.containers.list():
