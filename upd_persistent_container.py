3caab9bc1a61469ad6c9c67c51411a356500715e
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-09-26 09:43:51 +0530

    Update upd_persistent_container.py

--- upd_persistent_container.py
+++ upd_persistent_container.py
@@ -2,8 +2,10 @@ import os
 
 import docker
 
-full_file_name = 'openhands/runtime/plugins/agent_skills/file_ops/file_ops.py'
-full_file_name = r'openhands/runtime/client/client.py'
+full_file_names = [
+    'openhands/runtime/plugins/agent_skills/file_ops/file_ops.py',
+    r'openhands/runtime/client/client.py',
+]
 client = docker.from_env()
 for c in client.containers.list():
     if 'persisted' in c.name:
@@ -12,9 +14,10 @@ for c in client.containers.list():
         # copy the file to the container
         container_id = c.id
         print(container_id)
-        cmd = f'docker cp {full_file_name} {container_id}:/openhands/code/{full_file_name}'
-        print(cmd)
-        os.system(cmd)
+        for full_file_name in full_file_names:
+            cmd = f'docker cp {full_file_name} {container_id}:/openhands/code/{full_file_name}'
+            print(cmd)
+            os.system(cmd)
 
         if 'root' in c.name:
             break
