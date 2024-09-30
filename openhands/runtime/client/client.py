a8b73f291e607db38c272cd6f048b8f36cdc0da5
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-09-30 19:30:45 +0530

    protect test_task

--- openhands/runtime/client/client.py
+++ openhands/runtime/client/client.py
@@ -583,8 +583,12 @@ class RuntimeClient:
             # current working directory in Bash
             if self.pwd != getattr(self, '_jupyter_pwd', None):
                 await self.chdir()
-            if action.code == self.last_code and self.is_last_code_error:
+            if action.code.startswith('%%writefile /tmp/test_task.py'):
                 obs: Observation = ErrorObservation(
+                    "[The content in this file is absolutely correct. Also, you can't modify this test file. You must pass this test case. You should correct the codebase instead.]"
+                )
+            elif action.code == self.last_code and self.is_last_code_error:
+                obs = ErrorObservation(
                     '[You are trying to run the same code twice. Please focus and run the correct code.]'
                 )
             else:
