92aa2f22d4b3022d84d4397b746e7fde3f30d94b
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-10-01 17:36:37 +0530

    Update swe_bench_db_viewer.py

--- evaluation/swe_bench/swe_bench_db_viewer.py
+++ evaluation/swe_bench/swe_bench_db_viewer.py
@@ -1,30 +1,41 @@
 import os
 import pickle
 
+import toml
 from datasets import load_dataset
 
 from evaluation.swe_bench.swe_bench2 import update_issue_description
 
-instance_id = 'astropy__astropy-14539'
+with open(r'evaluation\swe_bench\config.toml', 'r') as f:
+    config = toml.load(f)
 
-if not os.path.exists('./cache/filtered_data.pkl') or 0:
-    dataset = load_dataset(
-        'princeton-nlp/SWE-bench_Verified',
-        cache_dir='./cache',
-        verification_mode='no_checks',
-        num_proc=4,
-        split='test',
-    )
-    # Serialize filtered dataset
-    ins = dataset.filter(lambda x: x['instance_id'] == instance_id)
-    with open('./cache/filtered_data.pkl', 'wb') as f:
-        pickle.dump(ins, f)
-else:
-    with open('./cache/filtered_data.pkl', 'rb') as f:
-        ins = pickle.load(f)
+# print(config)
+# instance_id = 'astropy__astropy-14539'
+instance_id = config['selected_ids'][0]
+force = 0
+for _ in range(2):
+    if not os.path.exists('./cache/filtered_data.pkl') or force:
+        dataset = load_dataset(
+            'princeton-nlp/SWE-bench_Verified',
+            cache_dir='./cache',
+            verification_mode='no_checks',
+            num_proc=4,
+            split='test',
+        )
+        # Serialize filtered dataset
+        ins = dataset.filter(lambda x: x['instance_id'] == instance_id)
+        with open('./cache/filtered_data.pkl', 'wb') as f:
+            pickle.dump(ins, f)
+        break
+    else:
+        with open('./cache/filtered_data.pkl', 'rb') as f:
+            ins = pickle.load(f)
+    if instance_id != ins['instance_id']:
+        force = 1
 
-print(ins['base_commit'])
-print(ins['environment_setup_commit'])
+#
+print('base_commit', ins['base_commit'])
+print('environment_setup_commit', ins['environment_setup_commit'])
 # print(ins['hints_text'])
 
 print(['problem_statement'])
