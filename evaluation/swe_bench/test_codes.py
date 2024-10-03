35a1eb97f4ad39e06d75c445c14ba8b1aa63b6cb
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-10-03 09:52:37 +0530

    Update test_codes.py

--- evaluation/swe_bench/test_codes.py
+++ evaluation/swe_bench/test_codes.py
@@ -58,9 +58,18 @@ print(fits.FITSDiff('diffbug.fits', 'diffbug.fits').identical)
 fits.printdiff('diffbug.fits', 'diffbug.fits')
 assert fits.FITSDiff('diffbug.fits', 'diffbug.fits').identical
 """,
+        'django__django-11133': r"""from django.conf import settings;
+settings.configure(DEFAULT_CHARSET='utf-8');
+from django.http import HttpResponse
+# memoryview content
+response = HttpResponse(memoryview(b"My Content"))
+print(response.content)
+
+assert response.content == b"My Content"
+""",
     }
     return test_codes.get(instance_id, '')
 
 
 if __name__ == '__main__':
-    print(get_test_code('astropy__astropy-14182'))
+    print(get_test_code('django__django-11133'))
