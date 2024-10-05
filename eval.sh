ebf265ef4bc8cac513a9778ece0653227ed649ad
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-10-05 16:07:30 +0530

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,22 +1,9 @@
-docker pull xingyaoww/sweb.eval.x86_64.astropy_s_astropy-12907:latest
 reset
-export SANDBOX_PERSIST_SANDBOX=1
-export model_config=llm.gemini_pro
-export model_config=llm.gemini
-export model_config=$1
-echo $model_config
-export USE_HINT_TEXT=true
+YOUR_OUTPUT_JSONL=evaluation/evaluation_outputs/outputs/swe-bench-lite/CodeActAgent/gemini-1.5-pro-exp-0827_maxiter_18_N_v1.9-no-hint/output.jsonl
+INSTANCE_ID=django__django-11179
+DATASET_NAME=princeton-nlp/SWE-bench_Verified
+SPLIT=test
 
-git_version=HEAD
-agent=CodeActAgent
-eval_limit=5
-export SELF_ANALYSE=0
-export IGNORE_COST=1
-export CONTINUE_ON_STEP=13
-max_iter=$((CONTINUE_ON_STEP + 15))
-num_workers=1
-dataset=princeton-nlp/SWE-bench_Lite
-split=test
-export SWE_BENCH=1
-./evaluation/swe_bench/scripts/run_infer.sh $model_config $git_version $agent $eval_limit $max_iter $num_workers $dataset $split
+./evaluation/swe_bench/scripts/eval_infer.sh $YOUR_OUTPUT_JSONL $INSTANCE_ID $DATASET_NAME $SPLIT
+
 # espeak 'e'
