a7c852e7412767e298afe18237feccaa2d7d4222
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-10-10 14:26:19 +0530

    gru_traj

--- evaluation/swe_bench/gru_traj_parser.py
+++ evaluation/swe_bench/gru_traj_parser.py
@@ -1,10 +1,11 @@
 import json
 
-from pyperclip import paste
-
-traj = json.loads(paste())
+with open('evaluation/swe_bench/gru_traj.json', 'r') as f:
+    traj = json.load(f)
 # // script to print chat human friendly
 for i in traj['trajectory']['details'][0]['exportedConversation']['plans'][0]:
     for j in i:
         print(j['thought'])
-        print('' * 100)
+        print(j['name'], j['args'])
+        print(j['observation'])
+        print('--' * 100)
