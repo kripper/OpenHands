4ad293220063681d4e413ae5a73ebecb58db5263
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-10-20 14:46:28 +0530

    remove constant for _clamp

--- openhands/runtime/plugins/agent_skills/file_ops/file_ops.py
+++ openhands/runtime/plugins/agent_skills/file_ops/file_ops.py
@@ -214,7 +214,7 @@ def open_file(
     output += _print_window(
         CURRENT_FILE,
         CURRENT_LINE,
-        _clamp(context_lines, 1, 300),
+        _clamp(context_lines, 1, total_lines),
         return_str=True,
         ignore_window=True,
     )
