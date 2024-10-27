42aef4ca4df32b099ba5e11503a696cd8e3435e1
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-10-27 18:20:05 +0530

    Update swe_verified_lite.py

--- evaluation/swe_bench/swe_verified_lite.py
+++ evaluation/swe_bench/swe_verified_lite.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 
 import toml
 from pyperclip import copy
@@ -383,11 +384,60 @@ r = [
 # print(len(r))
 # load config.toml
 
+non_gru_resolved = [
+    'astropy__astropy-12907',
+    'astropy__astropy-14182',
+    'astropy__astropy-14365',
+    'django__django-11848',
+    'django__django-11964',
+    'django__django-12125',
+    'django__django-13033',
+    'django__django-13158',
+    'django__django-13551',
+    'django__django-13590',
+    'django__django-13925',
+    'django__django-13964',
+    'django__django-14017',
+    'django__django-14155',
+    'django__django-14534',
+    'django__django-14580',
+    'django__django-14608',
+    'django__django-14787',
+    'django__django-15252',
+    'django__django-15695',
+    'matplotlib__matplotlib-23299',
+    'matplotlib__matplotlib-23476',
+    'matplotlib__matplotlib-24149',
+    'matplotlib__matplotlib-25311',
+    'pydata__xarray-4094',
+    'pylint-dev__pylint-7080',
+    'pytest-dev__pytest-7490',
+    'scikit-learn__scikit-learn-13496',
+    'scikit-learn__scikit-learn-14087',
+    'scikit-learn__scikit-learn-14983',
+    'scikit-learn__scikit-learn-25747',
+    'sphinx-doc__sphinx-11445',
+    'sympy__sympy-12419',
+    'sympy__sympy-12481',
+    'sympy__sympy-13031',
+    'sympy__sympy-15345',
+    'sympy__sympy-16792',
+    'sympy__sympy-17139',
+    'sympy__sympy-17630',
+    'sympy__sympy-17655',
+    'sympy__sympy-18189',
+    'sympy__sympy-18199',
+    'sympy__sympy-18698',
+    'sympy__sympy-20154',
+    'sympy__sympy-20590',
+    'sympy__sympy-21612',
+]
+
+pprint(non_gru_resolved)
 file = open('evaluation/swe_bench/config.toml', 'r')
 config = toml.load(file)
 instance = config['selected_ids'][0]
 
-r = sorted(r)
 # r= ['django__django-12308', 'django__django-12708', 'django__django-17087', 'matplotlib__matplotlib-24970', 'matplotlib__matplotlib-25332', 'pytest-dev__pytest-7432',  'sphinx-doc__sphinx-8595', 'sphinx-doc__sphinx-8721', 'sympy__sympy-21379', 'sympy__sympy-22714', 'sympy__sympy-24066']
 
 idx = r.index(instance)
