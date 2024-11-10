99bc4ec819664fae77c5905f0a38c2714996cbb4
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-11-10 13:05:45 +0530

    upd instruction

--- evaluation/swe_bench/run_infer.py
+++ evaluation/swe_bench/run_infer.py
@@ -86,8 +86,8 @@ def get_instruction(instance: pd.Series, metadata: EvalMetadata):
             'The current working directory is /testbed.\n'
             'Do not provide suggestions or workarounds. Directly fix the issue by modifying the source code.\n'
             "Plan:\n"
-            "*) Reproduce the issue in the test code before fixing it;\n"
-            # "*) Don't search for the user files in the repo because the user's code is an MRE (Minimal Reproducible Example) and wouldn't be part of the repository. It is verified that there is no issue in the user's code and this issue lies in the source code only. Focus only on modifying the existing repository code relevant to the issue instead. Search for the relevant files to modify using search_class, search_function and open_file agent skills instead of modifying the test files itself;\n"
+            # "*) Reproduce the issue in the test code before fixing it;\n"
+            "*) Don't search for the user files in the repo because the user's code is an MRE (Minimal Reproducible Example) and wouldn't be part of the repository. It is verified that there is no issue in the user's code and this issue lies in the source code only. Focus only on modifying the existing repository code relevant to the issue instead. Search for the relevant files to modify using search_class, search_function and open_file agent skills instead of modifying the test files itself;\n"
             "\n"
             'Add your valuable thoughts to every action you take.\n'
             # 'Determine the root cause of the issue and implement a direct fix, rather than employing a workaround.\n'
