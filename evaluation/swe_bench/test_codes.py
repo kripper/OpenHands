249b17df382142ce29f4a6e517573b4554749ef2
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-11-17 16:16:33 +0530

    Update test_codes.py

--- evaluation/swe_bench/test_codes.py
+++ evaluation/swe_bench/test_codes.py
@@ -278,6 +278,9 @@ if __name__ == '__main__':
         'django__django-13964': custom_test_code.format(
             cmd='tests/runtests.py  many_to_one.tests.ManyToOneTests.test_cached_foreign_key_with_to_field_not_cleared_by_save '
         ),
+        'django__django-14155': custom_test_code.format(
+            cmd='tests/runtests.py urlpatterns_reverse.tests.ResolverMatchTests.test_repr'
+        ),
     }
     return test_codes.get(instance_id, '')
 
