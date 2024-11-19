937a123ee9779029994d236fdfe695c906ce84b7
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-11-19 08:05:27 +0530

    add collapse button for terminal

--- frontend/src/components/container.tsx
+++ frontend/src/components/container.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "@remix-run/react";
 import clsx from "clsx";
 import React from "react";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+import IconButton from "./IconButton";
 
 function BetaBadge() {
   return (
@@ -28,6 +30,21 @@ export function Container({
   children,
   className,
 }: ContainerProps) {
+  const [isCollapsed, setIsCollapsed] = React.useState(false);
+
+  const handleCollapse = () => {
+    const workspace = document.querySelector(".workspace")?.parentElement as HTMLDivElement;
+    const terminal = document.querySelector(".Terminal")?.parentElement as HTMLDivElement;
+    if (workspace && terminal) {
+      workspace.style.height = "100%";
+      if (!isCollapsed) {
+        terminal.style.height = "22.9px";
+      } else {
+        terminal.style.height = "50%";
+      }
+      setIsCollapsed(!isCollapsed);
+    }
+  };
   return (
     <div
       className={clsx(
@@ -36,7 +53,7 @@ export function Container({
       )}
     >
       {labels && (
-        <div className="flex text-xs h-[36px]">
+        <div className="flex text-xs h-[36px] workspace">
           {labels.map(({ label: l, to, icon, isBeta }) => (
             <NavLink
               end
@@ -59,8 +76,16 @@ export function Container({
         </div>
       )}
       {!labels && label && (
-        <div className="px-2 h-[36px] border-b border-neutral-600 text-xs flex items-center">
+        <div className={"px-2 h-[36px] border-b border-neutral-600 text-xs flex items-center " + label}  >
           {label}
+          {label === "Terminal" && (
+            <IconButton
+              onClick={handleCollapse}
+              icon={isCollapsed ? <IoIosArrowUp /> : <IoIosArrowDown />}
+              ariaLabel={isCollapsed ? "Open Terminal" : "Close Terminal"}
+              style={{ marginLeft: "auto" }}
+            />
+          )}
         </div>
       )}
       <div className="overflow-auto h-full rounded-b-xl">{children}</div>
