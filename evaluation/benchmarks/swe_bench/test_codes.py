5a3063a5656e88f0810462cdddf044ab73b8276b
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-11-27 19:56:30 +0530

    Update test_codes.py

--- evaluation/benchmarks/swe_bench/test_codes.py
+++ evaluation/benchmarks/swe_bench/test_codes.py
@@ -120,12 +120,10 @@ assert result == sm.Matrix([[1, 0, 0, 2, 2, 0, 0, 0], [0, 1, 0, 2, 2, 0, 0, 0], 
         # MOD: (Piecewise((x, y > x), (y, True)) / z) % 1 added extra; not given in statement
         'sympy__sympy-21379': r"""
 from sympy import *
-from sympy.core.cache import clear_cache
 
 x, y, z = symbols('x y z', real=True)
 
-clear_cache()
-(Piecewise((x, y > x), (y, True)) / z) % 1
+# (Piecewise((x, y > x), (y, True)) / z) % 1
 expr = exp(sinh(Piecewise((x, y > x), (y, True)) / z))
 print(expr.subs({1: 1.0}))
 
