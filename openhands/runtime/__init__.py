251a4802daaafe87ba7eb366d1b0d8017cbb16c7
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-12-01 08:50:41 +0530

    Add Local Runtime

--- openhands/runtime/__init__.py
+++ openhands/runtime/__init__.py
@@ -3,6 +3,9 @@ from openhands.runtime.impl.e2b.sandbox import E2BBox
 from openhands.runtime.impl.eventstream.eventstream_runtime import (
     EventStreamRuntime,
 )
+from openhands.runtime.impl.eventstream.eventstream_local_runtime import (
+    LocalRuntime,
+)
 from openhands.runtime.impl.modal.modal_runtime import ModalRuntime
 from openhands.runtime.impl.remote.remote_runtime import RemoteRuntime
 from openhands.runtime.impl.runloop.runloop_runtime import RunloopRuntime
@@ -12,13 +15,14 @@ def get_runtime_cls(name: str):
     # Local imports to avoid circular imports
     if name == 'eventstream':
         return EventStreamRuntime
+    elif name == 'local':
+        return LocalRuntime
     elif name == 'e2b':
         return E2BBox
     elif name == 'remote':
         return RemoteRuntime
     elif name == 'ec2':
         from openhands.runtime.impl.ec2.ec2_runtime import EC2Runtime
-
         return EC2Runtime
     elif name == 'modal':
         logger.debug('Using ModalRuntime')
